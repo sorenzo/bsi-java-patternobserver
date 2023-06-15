4462f3671ce0fa41e729aa54056147a250751e0b
Sorenzo <sorenzo88@gmail.com>
2023-06-15 11:26:36 +0200

    Suppression du public dans les interfaces

--- src/main/java/org/example/obs_pattern/Observable.java
+++ src/main/java/org/example/obs_pattern/Observable.java
@@ -2,8 +2,8 @@ package org.example.obs_pattern;
 
 
 public interface Observable {
-    public void ajouter(Observer o);
-    public void retirer(Observer o);
-    public void notifier();
+    void ajouter(Observer o);
+    void retirer(Observer o);
+    void notifier();
 
 }
